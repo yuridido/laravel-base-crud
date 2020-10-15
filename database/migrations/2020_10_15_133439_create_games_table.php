6142551039f38ba76277963c068532bccd2fd735
yuridido <yuridido@yahoo.it>
2020-10-15 16:39:00 +0200

    creazione table

---
+++ database/migrations/2020_10_15_133439_create_games_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome_gioco',50);
+            $table->string('casa_editrice',50);
+            $table->smallInteger('min_gioc');
+            $table->smallInteger('max_gioc');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
